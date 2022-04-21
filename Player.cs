478d6c355a80b5f533fe04ada120383c2c84bb34
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-04-21 20:28:39 +0200

    feat: move/pickup objects

--- Player.cs
+++ Player.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static getObj;
+using static Inventory;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     public int health = 100;
     public GameObject itemInHand = null;
+    public Inventory inventory;
+
     void Start()
     {
         itemInHand = null;
+        inventory = new Inventory();
     }
 
     // Update is called once per frame
@@ -21,27 +25,32 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.E)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, distance)) {
-                print(hit.collider.gameObject.tag);
-                if (hit.collider.gameObject.tag == "ratio") {
-                    Destroy(hit.collider.gameObject);
+            GameObject obj = getObj.obj(ray, distance);
 
-                    gameObject.GetComponent<Inventory>().bullets += 1;
-                    print(gameObject.GetComponent<Inventory>().bullets);
+            if (Physics.Raycast(ray, out hit, distance)) {
+                print(obj.tag);
+                if (obj.tag == "ratio") {
+                    Destroy(obj);
+                    print("ajout inventaire");
+                    // inventory.add(IItems.RATIO);
                 }
             }
+            return;
         }
 
         if (Input.GetKeyUp(KeyCode.F)) {
+
+            if (itemInHand != null) {
+                itemInHand.transform.parent = null;
+                print("You have dropped " + itemInHand.name);
+                itemInHand = null;
+                return;
+            }
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             GameObject obj = getObj.obj(ray, distance);
+
             if (obj != null) {
-                if (itemInHand != null) {
-                    itemInHand.transform.parent = null;
-                    print("You have dropped " + itemInHand.name);
-                    itemInHand = null;
-                    return;
-                }
                 if (obj.tag == "ratio") {
                     itemInHand = obj;
                     obj.transform.SetParent(gameObject.transform, true);
