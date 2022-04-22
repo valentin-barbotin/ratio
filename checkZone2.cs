1bc0d6e6dd2e024020ee40bb0e5c22bca36d304b
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-04-22 11:52:45 +0200

    double ratio

--- checkZone2.cs
+++ checkZone2.cs
@@ -7,14 +7,14 @@ public class testzone : MonoBehaviour
     void OnTriggerEnter(Collider collider)
     {
         print("You have entered the zone");
-        print(collider.gameObject.name);
-        print(collider.gameObject.tag);
+        // print(collider.gameObject.name);
+        // print(collider.gameObject.tag);
     }
 
     void OnTriggerExit(Collider collider)
     {
         print("You have left the zone");
-        print(collider.gameObject.name);
-        print(collider.gameObject.tag);
+        // print(collider.gameObject.name);
+        // print(collider.gameObject.tag);
     }
 }
